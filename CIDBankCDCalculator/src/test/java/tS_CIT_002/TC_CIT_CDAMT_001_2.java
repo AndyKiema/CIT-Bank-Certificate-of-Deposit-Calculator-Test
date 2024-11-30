0076790d00a039049b9f5ac29ce1127c83b028c1
AndyKiema <andrewkiema94@gmail.com>
2024-11-30 22:43:16 +0300

    Valid test cases

---
+++ CIDBankCDCalculator/src/test/java/tS_CIT_002/TC_CIT_CDAMT_001_2.java
@@ -0,0 +1,47 @@
+package tS_CIT_002;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_CIT_CDAMT_001_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ChromeDriver cd= new ChromeDriver();
+	     cd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	     cd.get("https://www.cit.com/cit-bank/resources/calculators/certificate-of-deposit-calculator");
+	     cd.manage().window().maximize();
+	       
+
+	     WebElement initialamount=cd.findElement(By.xpath("//*[@id=\"mat-input-0\"]"));
+	     initialamount.clear();
+	     initialamount.sendKeys("14900");
+	        
+	     WebElement cdlength= cd.findElement(By.xpath("//*[@id=\"mat-input-1\"]"));
+	     cdlength.clear();
+	     cdlength.sendKeys("14");
+	       
+	     WebElement intrate= cd.findElement(By.xpath("//*[@id=\"mat-input-2\"]"));
+	     intrate.clear();
+	     intrate.sendKeys("23");
+	        
+	     cd.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]")).click();
+	     cd.findElement(By.xpath("//*[@id=\"mat-option-2\"]/span")).click();
+	     cd.findElement(By.xpath("//*[@id=\"CIT-chart-submit\"]/div")).click();
+	        
+	     String reflectedcdamt=cd.findElement(By.xpath("//*[@id=\"displayTotalValue\"]")).getText();
+	     String expectedamt="$19,341.70";
+	     System.out.println(reflectedcdamt);
+	     System.out.println(expectedamt);
+	     if(reflectedcdamt.equals(expectedamt)) {
+	        System.out.println("Test passed");
+	      }
+	      else {
+	        System.out.println("Test failed");
+	      }
+	}
+
+}

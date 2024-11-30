7573a521343bf57ce2333e5709fbb1501d6516fd
AndyKiema <andrewkiema94@gmail.com>
2024-11-30 22:54:41 +0300

    First invalid test case

--- CIDBankCDCalculator/src/test/java/tS_CIT_002/TC_CIT_CDAMT_001_5.java
+++ CIDBankCDCalculator/src/test/java/tS_CIT_002/TC_CIT_CDAMT_001_5.java
@@ -18,22 +18,22 @@ public class TC_CIT_CDAMT_001_5 {
 
 	    WebElement initialamount=cd.findElement(By.xpath("//*[@id=\"mat-input-0\"]"));
 	    initialamount.clear();
-	    initialamount.sendKeys("20400");
+	    initialamount.sendKeys("29000");
 	        
 	    WebElement cdlength= cd.findElement(By.xpath("//*[@id=\"mat-input-1\"]"));
 	    cdlength.clear();
-	    cdlength.sendKeys("28");
+	    cdlength.sendKeys("35");
 	       
 	    WebElement intrate= cd.findElement(By.xpath("//*[@id=\"mat-input-2\"]"));
 	    intrate.clear();
-	    intrate.sendKeys("45");
+	    intrate.sendKeys("56");
 	        
 	    cd.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]")).click();
-	    cd.findElement(By.xpath("//*[@id=\"mat-option-4\"]/span")).click();
+	    cd.findElement(By.xpath("//*[@id=\"mat-option-0\"]/span")).click();
 	    cd.findElement(By.xpath("//*[@id=\"CIT-chart-submit\"]/div")).click();
 	        
 	    String reflectedcdamt=cd.findElement(By.xpath("//*[@id=\"displayTotalValue\"]")).getText();
-	    String expectedamt="$48,546.23";
+	    String expectedamt="$78,578.06";
 	    System.out.println(reflectedcdamt);
 	    System.out.println(expectedamt);
 	    if(reflectedcdamt.equals(expectedamt)) {
